a78940e86d254e63b5c6262958b0837abd99583c
AquaApple <yassershehab9@gmail.com>
2022-05-01 17:26:40 +0200

    Check For Duplicate in a sentence and remove them

---
+++ js/Problems/Problem-4.js
@@ -0,0 +1,17 @@
+//Check For Duplicates and Remove Them
+
+//Solution
+function checkForDuplicate(words) {
+  const wordsArray = words.split(" ");
+  const noDuplicate = [];
+  let wordsObject = {};
+  for (let i = 0; i < wordsArray.length; i++) {
+    if (!wordsObject[wordsArray[i]]) {
+      wordsObject[wordsArray[i]] = true;
+      noDuplicate.push(wordsArray[i]);
+    }
+  }
+  return noDuplicate;
+}
+
+console.log(checkForDuplicate("My name is Yasser Yasser"));
