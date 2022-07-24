c70243f9e16358ee83c9ccc4c2c7dab8c0a82d0a
AquaApple <yassershehab9@gmail.com>
2022-07-24 20:11:47 +0200

    Longest Prefix

---
+++ js/Problems/Problem-9.js
@@ -0,0 +1,16 @@
+var longestCommonPrefix = function (strs) {
+  let letters = "";
+  let first = strs[0];
+  for (let i = 0; i < first.length; i++) {
+    let char = first[i];
+    for (let j = 0; j < strs.length; j++) {
+      if (strs[j][i] !== char) {
+        return letters;
+      }
+    }
+    letters += char;
+  }
+  return letters;
+};
+
+console.log(longestCommonPrefix(["flower", "flow", "flight", "flloooor", "fmmmm"]));
