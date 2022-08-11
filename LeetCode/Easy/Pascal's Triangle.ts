3f0b72855beb4a29d66962361e6e8c9f19acb022
AquaApple <yassershehab9@gmail.com>
2022-08-11 20:18:28 +0200

    Pascal's Triangle

---
+++ LeetCode/Easy/Pascal's Triangle.ts
@@ -0,0 +1,17 @@
+function generate(numRows: number): number[][] {
+  let pascalsTriangle = new Array(numRows);
+  for (let i = 0; i < numRows; i++) {
+    let row = new Array(i + 1);
+    row[0] = 1;
+    row[row.length - 1] = 1;
+
+    for (let j = 1; j < row.length - 1; j++) {
+      let rowAbove = pascalsTriangle[i - 1];
+      row[j] = rowAbove[j] + rowAbove[j - 1];
+    }
+    pascalsTriangle[i] = row;
+  }
+  return pascalsTriangle;
+}
+
+console.log(generate(7));
